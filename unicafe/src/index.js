454212cfa9095ab9d167995c1e257ef49df7b604
m2lovric <m2lovric@gmail.com>
2020-04-14 23:54:14 +0200

    exercises1.6update

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,22 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({text, handleClick}) => {
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <section>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </section>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -8,7 +24,12 @@ const App = () => {
 
   return(
     <div>
-    
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
